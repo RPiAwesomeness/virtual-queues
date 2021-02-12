d1fce39801eff468a223cc2f78e10f776541bfd9
RPiAwesomeness <keyboardmailtesting@gmail.com>
2021-02-12 14:58:50 -0500

    Complete React test

--- src/index.js
+++ src/index.js
@@ -79,6 +79,7 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -92,7 +93,7 @@ class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
