dc7b366de6ee20beb6d89b87dfc263c3ee77d46a
RPiAwesomeness <keyboardmailtesting@gmail.com>
2021-02-18 15:03:50 -0500

    Fixed header working

--- src/components/titlebar.js
+++ src/components/titlebar.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Button, Header, Icon } from "semantic-ui-react";
 
-class TitleBar extends React.Component {
+export default class TitleBar extends React.Component {
   constructor(props) {
     super(props);
     this.style = {
-      width: "100%",
-      minHeight: "10vh",
       display: "flex",
       justifyContent: "space-between",
       background: "#053864",
       padding: "20px",
       color: "white",
-      position: "fixed",
-      overflow: "hidden"
-    }
+      overflow: "hidden",
+    };
   }
 
   onHelpClicked() {
@@ -23,17 +20,15 @@ class TitleBar extends React.Component {
 
   render() {
     return (
-      <div style={this.style}>
-        <Button size="huge" animated="vertical" onClick={this.onHelpClicked}>
+      <header style={this.style} className="sticky">
+        <Button size="big" animated="vertical" onClick={this.onHelpClicked}>
           <Button.Content hidden>Help</Button.Content>
           <Button.Content visible>
             <Icon name="question circle outline" />
           </Button.Content>
         </Button>
         <Header as="h2" content="CU Events" subheader="Virtual Queues" />
-      </div>
+      </header>
     );
   }
 }
-
-export { TitleBar as default };
